a380d994d2ff25c051e09660f532c882a619bb8a
Ahmed Zubeyr Terzi <zubeyrterzi@gmail.com>
2022-10-30 22:13:51 +0300

    faction class oluştur

---
+++ main.py
@@ -0,0 +1,9 @@
+class Faction:
+    def __init__(self, name, number_of_units, health_point,attack_point, unit_regeneration_number):
+        self.name = name
+        self.number_of_units = number_of_units
+        self.health_point = health_point
+        self.attack_point = attack_point
+        self.unit_regeneration_number = unit_regeneration_number
+        self.total_health = self.number_of_units * self.health_point
+        self.is_alive = True
