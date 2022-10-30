287ba224ac5bd6698c9d54d3ad74458e22feabd3
Ahmed Zubeyr Terzi <zubeyrterzi@gmail.com>
2022-10-30 22:25:34 +0300

    define PerformAttack function for faction class

--- main.py
+++ main.py
@@ -10,3 +10,16 @@ class Faction:
     def AssgnEnemies(self, first_enemy, second_enemy):
         self.first_enemy = first_enemy
         self.second_enemy = second_enemy
+    def PerformAttack(self):
+        try:
+            if  self.first_enemy .is_alive ==True and self.second_enemy.is_alive == True:
+                self.first_enemy.ReceiveAttack(self, (self.attack_point/10)*5)
+                self.second_enemy.ReceiveAttack(self, (self.attack_point/10)*5)
+            elif self.first_enemy.is_alive ==True and self.second_enemy.is_alive == False:
+                self.first_enemy.ReceiveAttack(self, self.attack_point)
+            elif self.first_enemy.is_alive ==False and self.second_enemy.is_alive == True:
+                self.second_enemy.ReceiveAttack(self, self.attack_point)
+            else:
+                pass
+        except AttributeError:
+            print('Before calling this function, use AssgnEnemies function and assign enemies to function')
