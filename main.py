f277ac8836098741d634c4361edc5c19d770e4ef
Ahmed Zubeyr Terzi <zubeyrterzi@gmail.com>
2022-10-31 08:55:59 +0300

    define Dwarwes Class

--- main.py
+++ main.py
@@ -172,3 +172,47 @@ class Elves(Faction):
         merchant.weapon_point -= point_wanted
     def Print(self):
         return self.__dict__
+
+        
+class Dwarwes(Faction):
+    def __init__(self, name, number_of_units, health_point,attack_point, unit_regeneration_number):
+        super().__init__(name, number_of_units, health_point,attack_point, unit_regeneration_number)
+    def PerformAttack(self):
+        try:     
+            if self.first_enemy.is_alive ==True and self.second_enemy.is_alive == True:
+                self.first_enemy.ReceiveAttack(self, (self.attack_point/10)*5)
+                self.second_enemy.ReceiveAttack(self, (self.attack_point/10)*5)
+            elif self.first_enemy.is_alive ==True and self.second_enemy.is_alive == False:
+                self.first_enemy.ReceiveAttack(self, self.attack_point)
+            elif self.first_enemy.is_alive ==False and self.second_enemy.is_alive == True:
+                self.second_enemy.ReceiveAttack(self, self.attack_point)
+            else:
+                pass
+        except AttributeError:
+            print('Before calling this function, use AssgnEnemies function and assign enemies to function, Be sure that you called this function for all factions exists')
+
+    def ReceiveAttack(self, enemy, point):
+        if enemy == self.first_enemy:
+            point = point
+        else:
+            point = point
+        if self.total_health <= 0:
+            self.is_alive = False
+            print('The faction was destroyed')
+        elif point > self.total_health:
+            self.total_health = 0
+            self.is_alive = False
+            print('The faction was destroyed')
+        else:
+            self.total_health = self.total_health -point
+        self.number_of_units = self.total_health / self.health_point
+    def PurchaseWeapons(self, point_wanted, merchant):
+        self.attack_point += point_wanted
+        merchant.revenue += 10 * point_wanted
+        merchant.weapon_point -= point_wanted
+    def PurchaseArmors(self, point_wanted, merchant):
+        self.health_point += 2 * point_wanted
+        merchant.revenue +=  3 * point_wanted
+        merchant.weapon_point -= point_wanted
+    def Print(self):
+        return self.__dict__
